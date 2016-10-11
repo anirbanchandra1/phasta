38cf187aa309edd01018761fe6cd2718688cb4fa
Ehsan Shams <shamse@rpi.edu>
2016-10-11 18:09:25 -0400

    first commit on the getthm solid

--- phSolver/compressible/e3ivar_solid.f
+++ phSolver/compressible/e3ivar_solid.f
@@ -1,23 +1,35 @@
-      subroutine e3ivar_solid(g1yi_,g2yi_,g3yi_,rho_,npro_,nsd_)
+      subroutine e3ivar_solid(rho_, bulkMod_, shearMod_, Ja_def_, g1yi_,g2yi_,g3yi_,npro_,nsd_)
 c
         use solid_func_m
 c
         implicit none
 c
-        real*8, dimension(npro_,nsd_), target, intent(in) :: g1yi_,g2yi_,g3yi_
+        real*8, dimension(npro_), intent(out) :: bulkMod_, shearMod_
         real*8, dimension(npro_), target, intent(inout) :: rho_
+        real*8, dimension(npro_,nsd_), target, intent(in) :: g1yi_,g2yi_,g3yi_
+        real*8, dimension(npro_,nsd_), intent(in) :: Ja_def_
         integer, intent(in) :: npro_,nsd_
 c
         npro = npro_
         nsd = nsd_
 c
         rho => rho_
+c        ei  => ei_
+c        p   => p_
+c        T   => T_
+c        h   => h_
+c        cv  => cv_
+c        cp  => cp_
+c        alphaP => alphaP_
+c        betaT  => betaT_
+c        gamb  => gamb_
+c        c => c_
 c
         dudx => g1yi_(:,2:4)
         dudy => g2yi_(:,2:4)
         dudz => g3yi_(:,2:4)
 c
         call calc_solid
-        call getthm_solid_1
+        call getthm_solid_1(bulkMod_, shearMod_, Ja_def_)
 c
       end subroutine e3ivar_solid
--- phSolver/compressible/eqn_state_m.f
+++ phSolver/compressible/eqn_state_m.f
@@ -61,7 +61,38 @@ c        c =  sqrt(one/(rho_ref*betaT))
 c
       end subroutine getthm_liquid_1
 c
-      subroutine getthm_solid_1
+      subroutine getthm_solid_1(bulkMod, shearMod,Ja_def)
+c
+        real*8, dimension(npro), intent(out) :: bulkMod, shearMod
+        real*8, dimension(npro), intent(in)  :: Ja_def
+c
+        real*8 :: rho_ref_s, p_ref_s, T_ref_s, alpha_P_s, cv_s
+        real*8 :: bulkMod_s, shearMod_s 
+c
+        rho_ref_s = mat_prop(mater,iprop_solid_1_rho_ref,1)
+        p_ref_s   = mat_prop(mater,iprop_solid_1_p_ref,  1)
+        T_ref_s   = mat_prop(mater,iprop_solid_1_T_ref,  1)
+        cv_s     = mat_prop(mater,iprop_solid_1_cv,     1)
+        alpha_P_s = mat_prop(mater,iprop_solid_1_alphaP, 1)
+        bulkMod_s = mat_prop(mater,iprop_solid_1_bulkMod, 1)
+        shearMod_s = mat_prop(mater,iprop_solid_1_shearMod, 1)
+c        beta_T  = mat_prop(mater,iprop_solid_1_betaT,  1)
+c
+        alphaP = alpha_P_s
+        betaT  = one /(bulkMod_s * Ja_def) ! double check here
+        rho = rho_ref_s * (one - alphaP*(T - T_ref_s) 
+     &                    + betaT*(p - p_ref_s))
+c        ei  = ( cv_s - P * alpha_P_s/rho)* T 
+c     &        + (betaT * P - alphaP * T)/rho * p
+        ei  = cv_s * T
+        h   = ei + p/rho
+        cv  = cv_s
+        cp  = cv_s
+        bulkMod = bulkMod_s
+        shearMod = shearMod_s
+c        c =  sqrt(one/(rho_ref*betaT))
+C         c =  sqrt(one/(rho*betaT))
+C         gamb = zero
 c
 c
       end subroutine getthm_solid_1
