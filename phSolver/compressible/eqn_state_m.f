4b02286b1f1ca523fbc0f94c42fb17c6c8083827
Yu Zhang <zhangy41@rpi.edu>
2016-10-11 18:35:23 -0400

    Initialize the modification in eqn_state_m and solid_func_m

--- phSolver/compressible/eqn_state_m.f
+++ phSolver/compressible/eqn_state_m.f
@@ -74,10 +74,47 @@ c        c =  sqrt(one/(rho_ref*betaT))
 c
       end subroutine getthm_liquid_1
 c
-      subroutine getthm_solid_1(rho,npro)
 c
-        real*8, dimension(npro), intent(out) :: rho
-        integer, intent(in) :: npro
+      subroutine getthm_solid_1(rho,    ei
+     &,                           p,      T,     npro, mater
+     &,                           h,      cv,    cp
+     &,                           alphaP, betaT, bulkMod, shearMod
+     &,                           Ja_def)
+
+c
+        integer, intent(in) :: npro, mater
+        real*8, dimension(npro), intent(in)  :: p, T, Ja_def        
+        real*8, dimension(npro), intent(out) :: rho,ei,h
+        real*8, dimension(npro), intent(out) :: cv,cp,alphaP,betaT
+        real*8, dimension(npro), intent(out) :: bulkMod, shearMod
+c
+c
+        real*8 :: rho_ref_s, p_ref_s, T_ref_s, alpha_P_s, cv_s
+        real*8 :: bulkMod_s, shearMod_s 
+c
+        rho_ref_s = mat_prop(mater,iprop_solid_1_rho_ref,1)
+        p_ref_s   = mat_prop(mater,iprop_solid_1_p_ref,  1)
+        T_ref_s   = mat_prop(mater,iprop_solid_1_T_ref,  1)
+        cv_s     = mat_prop(mater,iprop_solid_1_cv,     1)
+        alpha_P_s = mat_prop(mater,iprop_solid_1_alphaP, 1)
+        bulkMod_s = mat_prop(mater,iprop_solid_1_bulkMod, 1)
+        shearMod_s = mat_prop(mater,iprop_solid_1_shearMod, 1)
+c
+        alphaP = alpha_P_s
+        betaT  = one /(bulkMod_s * Ja_def) ! double check here
+        rho = rho_ref_s * (one - alphaP*(T - T_ref_s) 
+     &                    + betaT*(p - p_ref_s))
+c        ei  = ( cv_s - P * alpha_P_s/rho)* T 
+c     &        + (betaT * P - alphaP * T)/rho * p
+        ei  = cv_s * T
+        h   = ei + p/rho
+        cv  = cv_s
+        cp  = cv_s
+        bulkMod = bulkMod_s
+        shearMod = shearMod_s
+c        c =  sqrt(one/(rho_ref*betaT))
+C         c =  sqrt(one/(rho*betaT))
+c
 c
       end subroutine getthm_solid_1
 c
--- phSolver/compressible/solid_func_m.f
+++ phSolver/compressible/solid_func_m.f
@@ -5,7 +5,11 @@ c
 c
         implicit none
 c
-        integer :: npro,nsd
+        integer :: npro_s, nsd_s
+        integer :: almBi_s, alfBi_s, gamBi_s
+        
+c
+        real*8  :: Delt_s
 c
         real*8, dimension(:), pointer :: rho
         real*8, dimension(:,:), pointer :: dudx, dudy, dudz
@@ -18,16 +22,99 @@ c
         allocate(AS(npro,6,6))
 c
         call calc_as_matrix
-c        call setB_af
-c        call get_det
-c        call get_det
+        d = almBi * bq + alfBi * Delt(1) * (almBi - gamBi) * bq_dot
+        call setB_af(d, AS, bq_af)
+        call get_det(bq_af,det_baf)
+        Ja_def= (det_baf)**0.5
+        call get_det
 c
         deallocate(AS)
+
 c
       end subroutine calc_solid
 c
-      subroutine calc_as_matrix
+      subroutine calc_as_matrix(dudx, dudy,dudz)
+c initialize the AS matrix
+         AS = zero !add
+c
+         AS(:,1,1) = 2 * dudx(:,1)
+         AS(:,1,5) = 2 * dudz(:,1)
+         AS(:,1,6) = 2 * dudy(:,1)
+c
+         AS(:,2,2) = 2 * dudy(:,2)
+         AS(:,2,4) = 2 * dudz(:,2)
+         AS(:,2,6) = 2 * dudx(:,2)
+c
+         AS(:,3,3) = 2 * dudz(:,3)
+         AS(:,3,4) = 2 * dudy(:,3)
+         AS(:,3,5) = 2 * dudx(:,3)
+c
+         AS(:,4,2) = dudy(:,3)
+         AS(:,4,3) = dudz(:,2)
+         AS(:,4,4) = dudy(:,2) + dudz(:,3)
+         AS(:,4,5) = dudx(:,2)
+         AS(:,4,6) = dudx(:,3)
+c
+         AS(:,5,1) = dudx(:,3)
+         AS(:,5,3) = dudz(:,1)
+         AS(:,5,4) = dudy(:,1)
+         AS(:,5,5) = dudx(:,1) + dudz(:,3)
+         AS(:,5,6) = dudy(:,3)
+c         
+         AS(:,6,1) = dudx(:,2)
+         AS(:,6,2) = dudy(:,1)
+         AS(:,6,4) = dudz(:,1)
+         AS(:,6,5) = dudz(:,2)
+         AS(:,6,6) = dudx(:,1) + dudy(:,2)
+c
 c
       end subroutine calc_as_matrix
+c
+      subroutine setB_af (d, AS, bq_af)
+c... calculate the left Cauchy-green tensor at time step n+af
+       implicit none 
+c
+       real*8, dimension(npro,6),intent(in) :: d
+       real*8, dimension(npro,6,6),intent(in) :: AS
+       integer,parameter :: nsize = 6 
+c
+       real*8, dimension(npro,6) :: bq_af
+       real*8, dimension(6,6) :: ident
+       real*8, dimension(6,6) :: temp_matrix
+c..................
+       ident = zero
+        do i = 1, nsize
+        ident(i,i) = one
+        enddo
+c
+        do i = 1, npro
+        temp_matrix(:,:) = almBi * ident(:,:) + gamBi * Delt(1)
+     &                     *alfBi * AS(i,:,:) !check here
+        bq_af(i,:) = matmul(temp_matrix(:,:) , d(i,:))
+        enddo
+c
+c
+       end subroutine setB_af 
+c
+c
+c
+c
+       subroutine get_det(matr,det_matr)
+c... calculate the determinant of 3 by 3 matrix
+      implicit none 
+c
+      real*8, dimension(npro,6),intent(in) :: matr
+c
+      real*8, dimension(npro) :: det_matr
+c..................
+      det_matr(:) = matr(:,1) * matr(:,2) * matr(:,3)
+     &+             matr(:,6) * matr(:,4) * matr(:,5)
+     &+             matr(:,5) * matr(:,6) * matr(:,4) 
+     &-             matr(:,1) * matr(:,4) * matr(:,4)
+     &-             matr(:,5) * matr(:,2) * matr(:,5)
+     &-             matr(:,6) * matr(:,6) * matr(:,3)
+c
+       end subroutine get_det
+c
 c
       end module solid_func_m
