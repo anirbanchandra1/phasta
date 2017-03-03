ce28a4dd7b9792cd078d923d4c6da559ac871889
Ehsan Shams <shamse@rpi.edu>
2017-03-03 12:14:36 -0500

    Correct way to loop on interface for umesh BC.

--- phSolver/compressible/if_velocity_m.f
+++ phSolver/compressible/if_velocity_m.f
@@ -39,7 +39,7 @@ c
         integer, intent(in)    :: lcblkif(14,MAXBLK+1), ilwork(nlwork)
         integer, intent(in) :: nshg, ndofBC, nsd, nelblif, MAXBLK, nlwork
 c
-        integer :: iblk, iel, npro,inode, inode0, inode1, n, ierr
+        integer :: iblk, iel, npro,inode, i0, i1, n, ierr
         integer, pointer :: ienif0(:,:), ienif1(:,:)
 
         if (numpe > 1) then
@@ -54,16 +54,32 @@ c
           call MPI_BARRIER (MPI_COMM_WORLD,ierr)
         endif
 c
-        do inode = 1,nshg
+c        do inode = 1,nshg
 c
 c ... NOT SURE IF THIS IS THE BEST IF :
 c
-          if (abs(sum_vi_area(inode,nsd+1)) > zero) then
-            umesh(inode,:) = sum_vi_area(inode,:) / sum_vi_area(inode,nsd+1)
-            BC(inode,:) = umesh(inode,:)
+c          if (abs(sum_vi_area(inode,nsd+1)) > zero) then
+c            umesh(inode,:) = sum_vi_area(inode,:) / sum_vi_area(inode,nsd+1)
+c            BC(inode,:) = umesh(inode,:)
 c      write(*,100) 'AFTER: ', myrank,inode, x(inode,:), sum_vi_area(inode,:),umesh(inode,:)
 c      write(*,200) 'AFTER: ', myrank,inode, umesh(inode,:)
-          endif
+c          endif
+c        enddo
+c
+        do iblk = 1,nelblif
+          npro  = lcblkif(1,iblk+1) - lcblkif(1,iblk)
+          ienif0 => mienif0(iblk)%p
+          ienif1 => mienif1(iblk)%p
+          do iel = 1,npro
+            do n = 1,3  ! only triangles on the surface
+              i0 = ienif0(iel,n)
+              i1 = ienif1(iel,n)
+              umesh(i0,:) = sum_vi_area(i0,:) / sum_vi_area(i0,nsd+1)
+              umesh(i1,:) = sum_vi_area(i1,:) / sum_vi_area(i1,nsd+1)
+              BC(i0,:) = umesh(i0,:)
+              BC(i1,:) = umesh(i1,:)
+            enddo
+          enddo
         enddo
 c
 100   format(a,'[',i2,'] ',i6,3f7.3,x,7e14.6)
