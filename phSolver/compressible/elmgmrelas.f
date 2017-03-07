e6bea91e9e9ef042e282ab9c1ab752ff8992e08b
Fan Yang <yangf4@rpi.edu>
2017-03-07 17:57:31 -0500

    elmgmrelas: normalize the growth curve normal

--- phSolver/compressible/elmgmrelas.f
+++ phSolver/compressible/elmgmrelas.f
@@ -183,7 +183,10 @@ c
           iflt = BLflt(ngc) ! first layer thickness for this growth curve
           igr  = BLgr(ngc)  ! growth ratio for this growth curve
           basevID = BLlist(listconuter + 1)
-          inormal = gcnormal(basevID,:)
+          tmp  = sqrt( gcnormal(basevID,1)**2
+                     + gcnormal(basevID,2)**2
+                     + gcnormal(basevID,3)**2 )
+          inormal = gcnormal(basevID,:) / tmp
 c
 c.... loop over vertices on this growth curve
 c
