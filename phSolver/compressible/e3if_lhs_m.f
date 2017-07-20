4c98e66798a11cfa81e88cf8ef27a83516ba1ef7
Anirban <anirbanchandra1@gmail.com>
2017-07-20 13:11:48 -0400

    old subroutines commented

--- phSolver/compressible/e3if_lhs_m.f
+++ phSolver/compressible/e3if_lhs_m.f
@@ -11,69 +11,69 @@ c
 c
       contains
 c
-      subroutine set_lhs_matrices
+c      subroutine set_lhs_matrices
 c
-        integer :: isd,jsd,n,p,q,r
+c        integer :: isd,jsd,n,p,q,r
 c
-        do q = 1,nflow
-          do p = 1,nflow
+c        do q = 1,nflow
+c          do p = 1,nflow
 c
-            A0Na_0(:,p,q) = zero
-            A0Na_1(:,p,q) = zero
+c            A0Na_0(:,p,q) = zero
+c            A0Na_1(:,p,q) = zero
 c
-            do n = 1,nshl0
-              A0Na_0(:,p,q) = A0Na_0(:,p,q) + A0_0(:,p,q)*shp0(:,n)
-            enddo
+c            do n = 1,nshl0
+c              A0Na_0(:,p,q) = A0Na_0(:,p,q) + A0_0(:,p,q)*shp0(:,n)
+c            enddo
 c
-            do n = 1,nshl1
-              A0Na_1(:,p,q) = A0Na_1(:,p,q) + A0_1(:,p,q)*shp1(:,n)
-            enddo
+c            do n = 1,nshl1
+c              A0Na_1(:,p,q) = A0Na_1(:,p,q) + A0_1(:,p,q)*shp1(:,n)
+c            enddo
 c
-            do isd = 1,nsd
+c            do isd = 1,nsd
 c
-              AiNa0  (:,isd,p,q) = zero
-              KijNaj0(:,isd,p,q) = zero
+c              AiNa0  (:,isd,p,q) = zero
+c              KijNaj0(:,isd,p,q) = zero
 c
-              do n = 1,nshl0
-                AiNa0  (:,isd,p,q) = AiNa0(:,isd,p,q) + Ai0(:,isd,p,q)*shp0(:,n)
-                do jsd = 1,nsd
-                  KijNaj0(:,isd,p,q) = KijNaj0(:,isd,p,q) + Kij0(:,isd,jsd,p,q)*shg0(:,n,jsd)
-                enddo
-              enddo
+c              do n = 1,nshl0
+c                AiNa0  (:,isd,p,q) = AiNa0(:,isd,p,q) + Ai0(:,isd,p,q)*shp0(:,n)
+c                do jsd = 1,nsd
+c                  KijNaj0(:,isd,p,q) = KijNaj0(:,isd,p,q) + Kij0(:,isd,jsd,p,q)*shg0(:,n,jsd)
+c                enddo
+c              enddo
 c
-              AiNa1  (:,isd,p,q) = zero
-              KijNaj1(:,isd,p,q) = zero
+c              AiNa1  (:,isd,p,q) = zero
+c              KijNaj1(:,isd,p,q) = zero
 c
-              do n = 1,nshl1
-                AiNa1  (:,isd,p,q) = AiNa1(:,isd,p,q) + Ai1(:,isd,p,q)*shp1(:,n)
-                do jsd = 1,nsd
-                  KijNaj1(:,isd,p,q) = KijNaj1(:,isd,p,q) + Kij1(:,isd,jsd,p,q)*shg1(:,n,jsd)
-                enddo
-              enddo
+c              do n = 1,nshl1
+c                AiNa1  (:,isd,p,q) = AiNa1(:,isd,p,q) + Ai1(:,isd,p,q)*shp1(:,n)
+c                do jsd = 1,nsd
+c                  KijNaj1(:,isd,p,q) = KijNaj1(:,isd,p,q) + Kij1(:,isd,jsd,p,q)*shg1(:,n,jsd)
+c                enddo
+c              enddo
 c
-            enddo
-          enddo
-        enddo
+c            enddo
+c          enddo
+c        enddo
 c
-        do q = 1,nflow
-          do p = 1,nflow
-            do isd = 1,nsd
+c        do q = 1,nflow
+c          do p = 1,nflow
+c            do isd = 1,nsd
 c
-              KijNajC0(:,isd,p,q) = zero
-              KijNajC1(:,isd,p,q) = zero
+c              KijNajC0(:,isd,p,q) = zero
+c              KijNajC1(:,isd,p,q) = zero
 c
-              do r = 1,nflow
-                KijNajC0(:,isd,p,q) = KijNajC0(:,isd,p,q) + KijNaj0(:,isd,p,r)*cmtrx(:,r,q)
-                KijNajC1(:,isd,p,q) = KijNajC1(:,isd,p,q) + KijNaj1(:,isd,p,r)*cmtrx(:,r,q)
-              enddo
+c              do r = 1,nflow
+c                KijNajC0(:,isd,p,q) = KijNajC0(:,isd,p,q) + KijNaj0(:,isd,p,r)*cmtrx(:,r,q)
+c                KijNajC1(:,isd,p,q) = KijNajC1(:,isd,p,q) + KijNaj1(:,isd,p,r)*cmtrx(:,r,q)
+c              enddo
 c
-            enddo
-          enddo
-        enddo
+c            enddo
+c          enddo
+c        enddo
 c
-        return
+c        return
 c
-      end subroutine set_lhs_matrices
+c      end subroutine set_lhs_matrices
 c
       subroutine calc_egmass_ (
      &                        egmass
@@ -158,117 +158,117 @@ c
 c
       end subroutine calc_egmass_
 c
-      subroutine calc_egmass( egmass00, egmass01,
-     &                         A0_0, A0_1, Ai0, Ai1,
-     &                         Kij0, Kij1,
-     &                         AiNa0_, AiNa1_, KijNaj0_, KijNaj1_,
-     &                         KijNajC0, KijNajC1,
-     &                         shp0, n0, n1, WdetJ0,
-     &                         prop,
-     &                         nshl0, nshl1)
+c      subroutine calc_egmass( egmass00, egmass01,
+c     &                         A0_0, A0_1, Ai0, Ai1,
+c     &                         Kij0, Kij1,
+c     &                         AiNa0_, AiNa1_, KijNaj0_, KijNaj1_,
+c     &                         KijNajC0, KijNajC1,
+c     &                         shp0, n0, n1, WdetJ0,
+c     &                         prop,
+c     &                         nshl0, nshl1)
 c        
-        real*8, dimension(:,:,:), intent(inout) :: egmass00, egmass01
-        real*8, dimension(:,:,:,:), intent(in)  :: AiNa0_, AiNa1_, KijNaj0_, KijNaj1_, KijNajC0, KijNajC1
-        real*8, dimension(:,:,:), intent(in):: A0_0,A0_1
-        real*8, dimension(:,:,:,:), intent(in) :: Ai0, Ai1
-        real*8, dimension(:,:,:,:,:), pointer, intent(in) :: Kij0, Kij1
-        real*8, dimension(:,:), intent(in) :: shp0,n0, n1
-        real*8, dimension(:), intent(in) :: WdetJ0
-        type(prop_t), dimension(:), pointer, intent(in) :: prop
-        integer, intent(in) :: nshl0,nshl1
+c        real*8, dimension(:,:,:), intent(inout) :: egmass00, egmass01
+c        real*8, dimension(:,:,:,:), intent(in)  :: AiNa0_, AiNa1_, KijNaj0_, KijNaj1_, KijNajC0, KijNajC1
+c        real*8, dimension(:,:,:), intent(in):: A0_0,A0_1
+c        real*8, dimension(:,:,:,:), intent(in) :: Ai0, Ai1
+c        real*8, dimension(:,:,:,:,:), pointer, intent(in) :: Kij0, Kij1
+c        real*8, dimension(:,:), intent(in) :: shp0,n0, n1
+c        real*8, dimension(:), intent(in) :: WdetJ0
+c        type(prop_t), dimension(:), pointer, intent(in) :: prop
+c        integer, intent(in) :: nshl0,nshl1
 c
-        integer :: i,j,p,q,inode0,inode1,isd,jsd
-        integer :: i0,j0,il,jl,iflow,jflow
-        real*8 :: this_mu(npro,nflow,nflow)
-        real*8 :: AiNa0(npro), KijNaj0(npro), KijNbjCn0(npro)
-        real*8 :: AiNa1(npro), KijNaj1(npro), KijNbjCn1(npro)
+c        integer :: i,j,p,q,inode0,inode1,isd,jsd
+c        integer :: i0,j0,il,jl,iflow,jflow
+c        real*8 :: this_mu(npro,nflow,nflow)
+c        real*8 :: AiNa0(npro), KijNaj0(npro), KijNbjCn0(npro)
+c        real*8 :: AiNa1(npro), KijNaj1(npro), KijNbjCn1(npro)
 c
 c---------> ATTENTION <--------
 c  This still needs to be confirmed...
 c
-      this_mu = zero
-      this_mu(:,2,2) = mu(:,2) ! mu
-      this_mu(:,3,3) = mu(:,3) ! "
-      this_mu(:,4,4) = mu(:,4) ! "
-      this_mu(:,5,5) = mu(:,5) ! kappa
+c      this_mu = zero
+c      this_mu(:,2,2) = mu(:,2) ! mu
+c      this_mu(:,3,3) = mu(:,3) ! "
+c      this_mu(:,4,4) = mu(:,4) ! "
+c      this_mu(:,5,5) = mu(:,5) ! kappa
 c
 c---------> END ATTANETION <-----
 c
-        do i = 1,nshl0
-          i0 = nflow*(i-1)
+c        do i = 1,nshl0
+c          i0 = nflow*(i-1)
 c
-          do j = 1,nshl0
-            j0 = nflow*(j-1)
+c          do j = 1,nshl0
+c            j0 = nflow*(j-1)
 c
-            do iflow = 1,nflow
-              do jflow = 1,nflow
+c            do iflow = 1,nflow
+c              do jflow = 1,nflow
 c
-                il = i0 + iflow
-                jl = j0 + jflow
+c                il = i0 + iflow
+c                jl = j0 + jflow
 c
-                AiNa0 = zero
-                KijNaj0 = zero
-                KijNbjCn0 = zero
+c                AiNa0 = zero
+c                KijNaj0 = zero
+c                KijNbjCn0 = zero
 c
-                do isd = 1,nsd
-                  AiNa0 = AiNa0 + pt50 * Ai0(:,isd,iflow,jflow) * n0(:,isd) * shp0(:,j)
-                  KijNbjCn0 = KijNbjCn0 + pt50 * s * KijNajC0(:,isd,iflow,jflow) * n0(:,isd)
-                  do jsd = 1,nsd
-                    KijNaj0 = KijNaj0 + pt50 * Kij0(:,isd,jsd,iflow,jflow) * shg0(:,j,jsd) * n0(:,isd)
-                  enddo
-                enddo
+c                do isd = 1,nsd
+c                  AiNa0 = AiNa0 + pt50 * Ai0(:,isd,iflow,jflow) * n0(:,isd) * shp0(:,j)
+c                  KijNbjCn0 = KijNbjCn0 + pt50 * s * KijNajC0(:,isd,iflow,jflow) * n0(:,isd)
+c                  do jsd = 1,nsd
+c                    KijNaj0 = KijNaj0 + pt50 * Kij0(:,isd,jsd,iflow,jflow) * shg0(:,j,jsd) * n0(:,isd)
+c                  enddo
+c                enddo
 c
-                egmass00(:,il,jl) = egmass00(:,il,jl)
-     &          - ( 
-     &            + AiNa0 
-     &            - KijNaj0
-     &            + KijNbjCn0 * shp0(:,j)
-CCc     &            + pt50 * A0_0(:,iflow,jflow)*alpha_LF*shp0(:,j)
+c                egmass00(:,il,jl) = egmass00(:,il,jl)
+c     &          - ( 
+c     &            + AiNa0 
+c     &            - KijNaj0
+c     &            + KijNbjCn0 * shp0(:,j)
+cCc     &            + pt50 * A0_0(:,iflow,jflow)*alpha_LF*shp0(:,j)
 cc     &            + pt50 * (A0_0(:,iflow,jflow)+A0_1(:,iflow,jflow))*alpha_LF*shp0(:,j)
-     &            + e*this_mu(:,iflow,jflow)/length_h * ctc(:,iflow,jflow) * shp0(:,j)
-     &            ) * shp0(:,i) * WdetJ0
-              enddo
-            enddo
+c     &            + e*this_mu(:,iflow,jflow)/length_h * ctc(:,iflow,jflow) * shp0(:,j)
+c    &            ) * shp0(:,i) * WdetJ0
+c             enddo
+c            enddo
 c
-          enddo
+c          enddo
 c
-          do j = 1,nshl1
-            j0 = nflow*(j-1)
+c          do j = 1,nshl1
+c            j0 = nflow*(j-1)
 c
-            do iflow = 1,nflow
-              do jflow = 1,nflow
+c            do iflow = 1,nflow
+c              do jflow = 1,nflow
 c
-                il = i0 + iflow
-                jl = j0 + jflow
+c                il = i0 + iflow
+c                jl = j0 + jflow
 c
-                AiNa1 = zero
-                KijNaj1 = zero
-                KijNbjCn0 = zero
+c                AiNa1 = zero
+c                KijNaj1 = zero
+c                KijNbjCn0 = zero
 c
-                do isd = 1,nsd
-                  AiNa1 = AiNa1 + pt50 * Ai1(:,isd,iflow,jflow) * n0(:,isd) * shp1(:,j)
-                  KijNbjCn0 = KijNbjCn0 + pt50 * s * KijNajC0(:,isd,iflow,jflow) * n0(:,isd)
-                  do jsd = 1,nsd
-                    KijNaj1 = KijNaj1 + pt50 * Kij1(:,isd,jsd,iflow,jflow) * shg1(:,j,jsd) * n0(:,isd)
-                  enddo
-                enddo
+c                do isd = 1,nsd
+c                  AiNa1 = AiNa1 + pt50 * Ai1(:,isd,iflow,jflow) * n0(:,isd) * shp1(:,j)
+c                  KijNbjCn0 = KijNbjCn0 + pt50 * s * KijNajC0(:,isd,iflow,jflow) * n0(:,isd)
+c                  do jsd = 1,nsd
+c                    KijNaj1 = KijNaj1 + pt50 * Kij1(:,isd,jsd,iflow,jflow) * shg1(:,j,jsd) * n0(:,isd)
+c                  enddo
+c                enddo
 c
-                egmass01(:,il,jl) = egmass01(:,il,jl)
-     &          - ( 
-     &            + AiNa1
-     &            - KijNaj1
-     &            - KijNbjCn0 * shp1(:,j)
+c                egmass01(:,il,jl) = egmass01(:,il,jl)
+c     &          - ( 
+c     &            + AiNa1
+c     &            - KijNaj1
+c     &            - KijNbjCn0 * shp1(:,j)
 CCc     &            - pt50 * A0_1(:,iflow,jflow)*alpha_LF*shp1(:,j)
 cc     &            - pt50 * (A0_0(:,iflow,jflow)+A0_1(:,iflow,jflow))*alpha_LF*shp1(:,j)
-     &            - e*this_mu(:,iflow,jflow)/length_h * ctc(:,iflow,jflow) * shp1(:,j)
-     &            ) * shp0(:,i) * WdetJ0
-              enddo
-            enddo
+c     &            - e*this_mu(:,iflow,jflow)/length_h * ctc(:,iflow,jflow) * shp1(:,j)
+c     &            ) * shp0(:,i) * WdetJ0
+c              enddo
+c            enddo
 c
-          enddo
+c          enddo
 c
-        enddo
+c        enddo
 c
-      end subroutine calc_egmass
+c      end subroutine calc_egmass
 c
       end module e3if_lhs_m
