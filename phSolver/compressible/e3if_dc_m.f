2bd9ae20506684e5cf7c53f7fb44a80604217b3b
Yu Zhang <zhangy41@rpi.edu>
2017-10-25 09:03:02 -0400

    initialize the discontinuity capturing module for interface

---
+++ phSolver/compressible/e3if_dc_m.f
@@ -0,0 +1,13 @@
+      module e3if_dc_m
+c
+c------------------------------------------------------------------------------
+c  calculating the discontinuity capturing (DC) operator for the interface
+c  macro-elements
+c------------------------------------------------------------------------------
+        inplicit none
+c
+c
+      contains
+      
+c
+      end module e3if_dc_m
